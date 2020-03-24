dc3aa80dc37b3f5f222299e35f63e8cdd61a50fe
Gian Alberto <galberto87@outlook.com>
2020-03-24 16:20:09 +0100

    opdracht1dag3

---
+++ Week2/Dag3/W2D3-1/basic.js
@@ -0,0 +1,26 @@
+// A: zet de functie om naar een arrow functie
+
+let ikRockArrowFunctions = () => console.log('Joe, ik rock de arrow functions!!');
+//____________________________________________
+
+// B: Schrijf de functie naar arrow op 1 regel.
+let fivePlusSeven = () => (5 + 7);
+
+//_____________________________________________
+// C: Schrijg zo kort mogelijk
+() => (1 + 2);
+
+//_____________________________________________
+// D: parameters a en b optellen
+
+let myFunction = (a, b) => (a + b);
+
+//_____________________________________________
+// E: parameters a en b optellen
+
+let addFive = a => (a + 5);
+
+//_____________________________________________
+// F: 
+
+let createObject = () => ('greeting: "hoi"');
