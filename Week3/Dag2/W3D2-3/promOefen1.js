f8ecb936e43ca6165b54315e7a63aefa3c86a7c4
Gian Alberto <galberto87@outlook.com>
2020-03-31 09:54:53 +0200

    Update di 0954

--- Week3/Dag2/W3D2-3/promOefen1.js
+++ Week3/Dag2/W3D2-3/promOefen1.js
@@ -6,33 +6,47 @@ greater than the value 10. Log the result to the console.
 */
 
 
-function testNum(x) {
+// function testNum(x) {
     
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-        if (x < 10) {
-            resolve("Getal is kleiner dan 10");
-        } else {
-            reject("Getal is groter dan 10"); 
-        } 
-        });  
-        });
-    }
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//         if (x < 10) {
+//             resolve("Getal is kleiner dan 10");
+//         } else {
+//             reject("Getal is groter dan 10"); 
+//         } 
+//         });  
+//         });
+//     }
 
-testNum(2)
-.catch(xNr => console.log(xNr));
+// testNum(2)
+// .catch(xNr => console.log(xNr));
 
   
 /*
 Exercise 2:
-Write two functions that use Promises that you can chain! The first function, makeAllCaps(), will take in an array of words and capitalize them, and then the second function, sortWords(), will sort the words in alphabetical order. If the array contains anything but strings, it should throw an error.
+Write two functions that use Promises that you can chain! The first function, makeAllCaps(), 
+will take in an array of words and capitalize them, and then the second function, sortWords(), will sort the words in alphabetical order. 
+If the array contains anything but strings, it should throw an error.
 Then call these functions by *chaining* the promises
 */
 
-// const makeAllCaps = () => {};  
-// const sortWords = () => {};
+// const makeAllCaps = () => {
+//     let x = arrayOfWords.lenght
+//     return x.toUpperCase();
+// }; 
+const arrayOfWords = ['cucumber', 'tomatos', 'avocado'];
+let makeAllCaps = arrayOfWords.map(function toUpper(item) {
+    return item.toUpperCase();
+});
 
-// const arrayOfWords = ['cucumber', 'tomatos', 'avocado'];
-// const complicatedArray = ['cucumber', 44, true];
+
+
+const sortWords = () => {};
+
+
+const complicatedArray = ['cucumber', 44, true];
 
 // call both functions, chain them together and log the result to the console
+
+makeAllCaps();
